6af6deb38abfc1ae4f6e6e84885b47ab52a242c2
allegras066 <allegrasantoso@gmail.com>
2024-09-24 15:16:06 +0700

    Made Template Components

---
+++ src/app/components/movielist.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function movielist() {
+  return <div>movielist</div>;
+}
+
+export default movielist;
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+function navbar() {
+  return (
+    <div className="">
+      <Image></Image>
+      <button></button>
+    </div>
+  );
+}
+
+export default navbar;
